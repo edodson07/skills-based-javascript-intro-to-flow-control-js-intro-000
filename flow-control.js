ff26f4ff41bff70ca93671275b80ebd3b2731560
Eric Dodson <edodson958@gmail.com>
2020-07-14 20:17:44 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,7 +32,7 @@ function ageChecker(age)
 
   }
 
-function ternaryTeenager(age) 
+function ternaryTeenager(age)
 
 {
   return (age <= 19 && age >= 13 ) ? "You are a teenager!" : "You are not a teenager"
