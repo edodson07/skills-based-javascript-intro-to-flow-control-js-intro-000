c85411ee28c30f1209e059f6864f6977f8a0c9dd
Eric Dodson <edodson958@gmail.com>
2020-07-14 20:11:20 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,16 +14,23 @@ function teenager(age)
     if(age <= 19 && age >= 13 )                 // True Statement
       {  return "You are a teenager!"}
 
-    else if ( age > 19 || age < 13 )          // False Statement
+    else if ( age > 19 || age < 13 )            // False Statement
       { return "You are not a teenager"}
-    //else                                        // false
-      //{ return "You are not a teenager"}
-
+      
   }
 
-function ageChecker(age) {
+function ageChecker(age) 
+  {
+    if(age <= 19 && age >= 13 )                 // True Statement
+      {  return "You are a teenager!"}
 
-}
+    else if ( age <= 12)                       // False Statement 1  - kid
+      { return "You are a kid"}
+      
+    else if ( age > 19 )                       // False Statement 2 - grown up 
+        { return "You are a grownup"}    
+      
+  }
 
 function ternaryTeenager(age) {
 
